efca8545eb2caaa549661932898e476efd68a9ae
kchambit <kev132011@hotmail.com>
2023-04-12 10:55:18 -0500

    Swagger documentation, auth in API docs

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
+    .addBearerAuth()
     .setTitle('CROKO API')
     .setDescription('YT Course')
     .setVersion('1.0')
     .addTag('items')
+    .addTag('auth')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('documentation', app, document);
